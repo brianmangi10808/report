ddee6eb1ac5c9e33c34661210fe36db403e775a6
Chief <brianmwangi10808@gmail.com>
2024-04-10 14:58:20 +0300

    commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
       <h2>About us</h2>
       <p>Welcome to Report,a dedicated platform for women to share their experiences and contribute to building safer environments.At Report we believe in transparency ,accoutability and empowerment </p>
 
-      <h2>Our mission</h2>
-      <p>Our mission is to provide a safe and confidential space for  women to share their experiences of harassment, discrimination, or any form of misconduct. By collecting and documenting these incidents with verified identities, we aim to create a comprehensive database that can be accessed by women seeking information about the safety record of particuar environment.</p>
+      {/* <h2>Our mission</h2>
+      <p>Our mission is to provide a safe and confidential space for  women to share their experiences of harassment, discrimination, or any form of misconduct. By collecting and documenting these incidents with verified identities, we aim to create a comprehensive database that can be accessed by women seeking information about the safety record of particuar environment.</p> */}
 
       <h2>How It Works</h2>
       <p>To contribute to our database, users must submit their experiences with valid identification and details . Each submission is carefully reviewed, and appropriate action is taken to address reported issues. We are committed to maintaining the privacy and confidentiality of all users while promoting transparency and accountability in  environments.</p>
