e36289073579bc1c00ac11d2b0ca3032fd50f95f
Chief <brianmwangi10808@gmail.com>
2024-04-10 09:28:17 +0300

    commit

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,12 +1,13 @@
 
 import React from 'react'
+import './Report.css'
 
 const Report = () => {
   return (
     <div className='report'>
       <div className="report-container">
         <h1>Report Page</h1>
-        <form action="">
+        <form className='form-container'>
             <input type="text"  placeholder="Area to Report"/>
 
             <input type="text" name="name" placeholder="Your name" />
@@ -14,7 +15,7 @@ const Report = () => {
             <textarea name="" id="" cols="30" rows="10" placeholder="Write your experience here ..."></textarea>
 
             <button>Submit</button>
-            
+
         </form>
       </div>
     </div>
